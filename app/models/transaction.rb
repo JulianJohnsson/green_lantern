f5addaf9626c29c7c9e36064c98968645915a8be
Julien Janson <juljanson@gmail.com>
2019-08-28 18:15:28 +0200

    view by category

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,7 +4,10 @@ class Transaction < ApplicationRecord
 
   scope :recent, -> {where("date > ?", 1.month.ago)}
   scope :month_to_date, -> {where("date >= ?", Date.today.beginning_of_month)}
+
   scope :parent_category_id, -> (category_id) {where("parent_category_id = ?", category_id)}
+  scope :category_id, -> (category_id) {where("category_id = ?", category_id)}
+
   scope :month_ago, -> (int) {where("date >= ? AND date <= ?", int.months.ago.beginning_of_month, int.months.ago.end_of_month)}
   scope :carbone_contribution, -> {where "carbone > 0"}
 
