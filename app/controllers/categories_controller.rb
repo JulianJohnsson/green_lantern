ebb34f4eb616ff71560e2a4d792391c263d5100e
Julien Janson <juljanson@gmail.com>
2019-09-09 18:08:38 +0200

    Compare page draft

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,4 +12,10 @@ class CategoriesController < ApplicationController
     # @carbone_by_subcategory = Category.all.find_by_parent_id(@category.external_id).map {|c| [c.name, current_user.transactions.carbone_contribution.find_by_external_id(c.external_id).month_ago(params[:month].to_i || 0).sum(:carbone)]}
     @transactions = current_user.transactions.carbone_contribution.parent_category_id(@category.external_id).month_ago(params[:month].to_i || 0)
   end
+
+  def compare
+    @categories = Category.all.parent_categories
+    @carbone_by_category = @categories.sort_by {|c| c.name}.map {|c| [c.name , current_user.transactions.carbone_contribution.parent_category_id(c.external_id).month_ago(params[:month].to_i || 0).sum(:carbone)]}
+    @avg_carbone_by_category = @categories.sort_by {|c| c.name}.map {|c| [c.name , Transaction.all.carbone_contribution.parent_category_id(c.external_id).month_ago(params[:month].to_i || 0).sum(:carbone) / Transaction.all.carbone_contribution.parent_category_id(c.external_id).month_ago(params[:month].to_i || 0).distinct.count(:user_id)]}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get '/bankin' => 'bridges#index'
   get '/account' => 'bridges#account'
   get '/dashboard' => 'transactions#dashboard'
+  get '/compare' => 'categories#compare'
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
