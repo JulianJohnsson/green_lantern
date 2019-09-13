617ac013cbe5f23c415e19f3363d5dcf42cf00c7
Julien Janson <juljanson@gmail.com>
2019-09-13 16:16:05 +0200

    Dashboard design fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,7 +56,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :city, :birthdate, preferences_attributes: [:user_id, :city, :regime, :energy_contract]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :city, :birthdate, preferences_attributes: [:id, :user_id, :city, :regime, :energy_contract]])
   end
 
   # The path used after sign up.
