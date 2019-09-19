341e81ff6c0a686223cf9bfa349ff9252df65cd2
Julien Janson <juljanson@gmail.com>
2019-09-19 11:15:18 +0200

    public projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,13 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @projects = Project.all
+  end
+
+  def new
+    @projects = Project.all
+    render :layout => 'pages'
   end
 
 end
