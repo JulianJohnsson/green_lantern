953192b205df562fa648fed0ddc956ba9f3e097a
Julien Janson <juljanson@gmail.com>
2019-11-20 16:35:33 +0100

    update stripe js

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-  const publishableKey = document.querySelector("meta[name='stripe-key']").content;
-  const stripe = Stripe(publishableKey);
+  var publishableKey = document.querySelector("meta[name='stripe-key']").content;
+  var stripe = Stripe(publishableKey);
 
-  const elements = stripe.elements({
+  var elements = stripe.elements({
     fonts: [{
       cssSrc: "https://rsms.me/inter/inter-ui.css"
     }],
     locale: "auto"
   });
 
-  const style = {
+  var style = {
     base: {
       color: "#32325d",
       fontWeight: 500,
@@ -26,12 +26,12 @@ $(document).ready(function(){
     }
   };
 
-  const card = elements.create('card', { style });
+  var card = elements.create('card', { style });
 
   card.mount("#card-element");
 
   card.addEventListener('change', ( {error} ) => {
-    const displayError = document.getElementById('card-errors');
+    var displayError = document.getElementById('card-errors');
     if(error) {
       displayError.textContent = error.message;
     } else {
@@ -39,15 +39,15 @@ $(document).ready(function(){
     }
   });
 
-  const form = document.getElementById('payment-form');
+  var form = document.getElementById('payment-form');
 
   form.addEventListener('submit', async(event) => {
     event.preventDefault();
 
-    const { token, error } = await stripe.createToken(card);
+    var { token, error } = await stripe.createToken(card);
 
     if (error) {
-      const errorElement = document.getElementById('card-errors');
+      var errorElement = document.getElementById('card-errors');
       errorElement.textContent = error.message;
     } else {
       stripeTokenHandler(token);
@@ -55,9 +55,9 @@ $(document).ready(function(){
   });
 
 
-  const stripeTokenHandler = (token) => {
-    const form = document.getElementById('payment-form');
-    const hiddenInput = document.createElement('input');
+  var stripeTokenHandler = (token) => {
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
     hiddenInput.setAttribute('type', 'hidden');
     hiddenInput.setAttribute('name', 'stripeToken');
     hiddenInput.setAttribute('value', token.id);
