4454dcc6c3e86cb8ddc287d3ea7e365b3515e6de
Julien Janson <juljanson@gmail.com>
2019-12-10 15:00:59 +0100

    publish new reduction page

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
 
   get '/track' => 'categories#track'
   get '/compare_with' => 'matches#index'
-  get '/reduce' => 'categories#reduce'
+  get '/reduce' => 'reductions#index'
   get '/compare', to: redirect('/reduce', status: 301)
 
   get '/faq' => 'high_voltage/pages#show', id: 'faq'
