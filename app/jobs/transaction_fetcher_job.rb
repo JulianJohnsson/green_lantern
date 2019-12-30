be33bc1198e11eb83835df450887750c7415c2ce
Julien Janson <juljanson@gmail.com>
2019-12-30 19:06:31 +0100

    remove bugging event for GA

--- app/jobs/transaction_fetcher_job.rb
+++ app/jobs/transaction_fetcher_job.rb
@@ -4,7 +4,6 @@ class TransactionFetcherJob < ApplicationJob
   def perform(user)
     @bridge = Bridge.find_by_user_id(user.id)
     @bridge.create_transactions(user)
-    AnalyticService.new.track('Transactions Synced', nil, user)
     if user.transactions.present?
       @score = Score.find_by_user_id(user.id)
       @score.kind = :dynamic
