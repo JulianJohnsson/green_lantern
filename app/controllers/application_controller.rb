6c54b1f6c1af709ad86558bee42065fa1abce181
Julien Janson <juljanson@gmail.com>
2020-01-06 18:38:13 +0100

    pimped invitation email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name])
+    devise_parameter_sanitizer.permit(:invite, keys: [:name])
   end
 
   private
