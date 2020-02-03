2c0bcc37bf2df192c54838b35d4239664673080f
Julien Janson <juljanson@gmail.com>
2020-02-03 13:51:14 -0600

    update category select and transaction to categorize

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ApplicationRecord
   has_many :transactions
+  belongs_to :parent, :class_name => 'Category', optional: true
+  has_many :children, -> { order(:name) }, :class_name => 'Category', :foreign_key => 'parent_id'
 
+  after_update :update_transactions, if: :saved_change_to_coeff?
+
+  scope :root_categories, -> {where("id IN (1,12,25,25,70,136)")}
   scope :parent_categories, -> {where("parent_id=0 AND coeff>0")}
   scope :sub_categories, -> (int) {where("parent_id = ?", int)}
 
@@ -40,6 +45,13 @@ class Category < ApplicationRecord
   end
 
   def self.top_growth_advice(score)
+  end
+
+  def update_transactions
+    transactions = self.transactions
+    transactions each do |t|
+      t.save
+    end
   end
 
 end
