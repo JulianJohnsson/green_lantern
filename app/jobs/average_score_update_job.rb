cc7a77ae3f84c73aa5b15a2362690f3b56053da4
Julien Janson <juljanson@gmail.com>
2020-04-14 10:32:10 +0200

    set weekly score in average score update job

--- app/jobs/average_score_update_job.rb
+++ app/jobs/average_score_update_job.rb
@@ -2,6 +2,8 @@ class AverageScoreUpdateJob < ApplicationJob
 
   def perform
     AverageScore.create_average(0.day.ago)
+    @avg = AverageScore.all.where("score_kind = 1").last
+    @avg.update_week_history
   end
 
 end
