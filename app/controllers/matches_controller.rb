fb2a7629c8c729e9d1c7cf8f657775dcd02aa786
Julien Janson <juljanson@gmail.com>
2020-04-17 18:10:33 +0200

    WIP trends project

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -58,15 +58,14 @@ class MatchesController < ApplicationController
     @project = (current_user.subscription_price||0) * -50
 
     @array = []
-    @scores = Score.all.dynamic
+    @scores = Score.all.dynamic.where("recent_total > 0")
     @scores.each do |score|
       user = score.user
       name = user.name
       points = user.scores.last.recent_total*1000/12 + (user.set_level[2]||0) * -10 + ((User.where("invited_by_id = ?", user.id).count)||0) * -20 + (user.subscription_price||0) * -50
       @array << [name, points]
     end
-    @ranking = @array
-    #.sort { |a,b| a[1]>b[1] }
+    @ranking = @array.sort { |a,b| a[1]>=b[1] }
   end
 
 end
