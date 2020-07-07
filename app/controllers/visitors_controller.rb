009af18327c764e445a4e5553935757a7a57512c
Julien Janson <juljanson@gmail.com>
2020-07-07 23:13:33 +0200

    fix calculation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,6 @@
 class VisitorsController < ApplicationController
   def index
     @average_history = AverageScore.where("score_kind = 1").sort_by {|c| c.created_at}
-    @average_history.each do |average|
-      average.week_total = average.week_total.to_f * 1000 / 12
-      for i in 0..4
-        average.week_detail[i] = average.week_detail[i].to_f * 1000 /12
-      end
-    end
     @categories = Category.all.parent_categories.sort_by {|c| c.id}
 
   end
