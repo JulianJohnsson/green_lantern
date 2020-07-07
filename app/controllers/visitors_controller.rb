bce0e8ef251d1a87b1ad01f86fc4f44e364f04f8
Julien Janson <juljanson@gmail.com>
2020-07-07 23:00:37 +0200

    add trends

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
   def index
-    @average_history = AverageScore.where("score_kind = 1")
+    @average_history = AverageScore.where("score_kind = 1").sort_by {|c| c.created_at}
     @average_history.each do |average|
       average.week_total = average.week_total.to_f * 1000 / 12
       for i in 0..4
