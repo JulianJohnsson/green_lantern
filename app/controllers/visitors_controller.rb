79d09e177addc223cc6e927b2eb9549c5df877d1
Julien Janson <juljanson@gmail.com>
2020-07-30 15:24:50 +0200

    page impact carbone

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,9 @@ class VisitorsController < ApplicationController
   def index
     @average_history = AverageScore.where("score_kind = 1").sort_by {|c| c.created_at}
     @categories = Category.all.parent_categories.sort_by {|c| c.id}
+    @week = @average_history.last
+    @rand = rand(3)
+    render :layout => 'pages'
 
   end
 end
