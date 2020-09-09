dd548b05b6ecd5d73228e05c83d41158a11a4872
Julien Janson <juljanson@gmail.com>
2020-09-09 09:16:30 +0200

    update indice page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,11 @@ class VisitorsController < ApplicationController
     @average_history = AverageScore.where("score_kind = 1").sort_by {|c| c.created_at}
     @categories = Category.all.parent_categories.sort_by {|c| c.id}
     @week = @average_history.last
-    @rand = rand(3)
+    @previous = @average_history.last(2).first
+    @data = []
+    for i in 0..4
+      @data = @data << [@categories[i].name, (@week.week_detail[i].to_f*1000/12).round(2)]
+    end
     render :layout => 'pages'
-
   end
 end
