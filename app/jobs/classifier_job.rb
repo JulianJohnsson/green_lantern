1ccae21fe4408401dff5cc7378ad12b7ddaae6ef
Julien Janson <juljanson@gmail.com>
2020-10-07 15:31:07 +0200

    bla

--- app/jobs/classifier_job.rb
+++ app/jobs/classifier_job.rb
@@ -2,6 +2,7 @@ class ClassifierJob < ApplicationJob
 
   require 'classifier-reborn'
 
+
   def perform
     transactions = Transaction.all.where("description is not null and category_id is not null")
     @lsi = ClassifierReborn::LSI.new(auto_rebuild: false)
