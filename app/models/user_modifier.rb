d1a9b5936515e7fe841e8537aa1a396b90a25d89
Julien Janson <juljanson@gmail.com>
2020-10-12 18:07:58 +0200

    fix select2 on categorize page

--- app/models/user_modifier.rb
+++ app/models/user_modifier.rb
@@ -7,16 +7,20 @@ class UserModifier < ApplicationRecord
   def self.food(user)
     score = user.scores.last
     if score.redmeat == nil && score.poultry == nil && score.dairy == nil
-      case score.regime.to_sym when :moyen
+      if score.regime.present?
+        case score.regime.to_sym when :moyen
+          modifier = 0
+        when :végétalien
+          modifier = -0.463
+        when :végétarien
+          modifier = -0.3955
+        when :flexitarien
+          modifier = -0.2553
+        when :viandard
+          modifier = 0.5093
+        end
+      else
         modifier = 0
-      when :végétalien
-        modifier = -0.463
-      when :végétarien
-        modifier = -0.3955
-      when :flexitarien
-        modifier = -0.2553
-      when :viandard
-        modifier = 0.5093
       end
     else
       modifier = (((score.redmeat || 0) - 3) * 0.14 + ((score.poultry || 0) - 4) * 0.053 + ((score.dairy || 0) - 4) * 0.017) / Country.find(score.country_id).detail[4].to_f
